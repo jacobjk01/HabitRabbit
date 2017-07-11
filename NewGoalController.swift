530fd5ea08d6022cd9078d2290140f9b6eef6ae3
19jacobk <jacobjk01@gmail.com>
2017-07-10 17:02:15 -0700

    Added new goal storyboard

---
+++ NewGoalController.swift
@@ -0,0 +1,28 @@
+//
+//  NewGoalController.swift
+//  HabitTrack
+//
+//  Created by Jacob Kim on 7/10/17.
+//  Copyright © 2017 Jacob Kim. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class NewGoalController: UIViewController {
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+//    let view:UIView = .view
+//    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        
+//        scrollView = UIScrollView(frame: view.bounds)
+//        scrollView.contentSize = imageView.bounds.size
+//        scrollView.autoresizingMask = UIViewAutoresizing.flexibleWidth;UIViewAutoresizing.flexibleHeight
+//        
+//        scrollView.addSubview(imageView)
+//        view.addSubview(scrollView)
+//    }
+}
